44a6316388ba451d878f73cb790543d3e976b0d9
Sandras <sandras.shimanauskas@gmail.com>
2017-09-16 01:16:36 +0300

    Minor work

--- x86/MBR.s
+++ x86/MBR.s
@@ -1,47 +1,45 @@
-; MBR.S - Master Boot Record in Assembly.
+; MBR.S - master boot record.
 
-%MACRO	check 1
-	cmp byte [7A00h+%1], 80h
-	jne $+7
+org	0x7A00				; We are actually loaded at 0x7C00 but we will relocate to 0x7A00 before any position dependent code.
+bits	16
+cpu	8086				; We want to be compatible with the oldest of PCs.
 
-	inc cl
-	mov si, 7A00h+%1
+%macro	check 1
+	cmp byte [0x7A00+%1], 0x80	; Check for active partition flag.
+	jne $+7				; If active.
+	inc cl				; Count of active partitions.
+	mov si, 0x7A00+%1		; Memorize this partition in case it's the only one marked active.
 %endmacro
 
-org	7A00h
-bits	16
-cpu	8086
-
-	cli						; Clear interrupts. Re-enable them once the IDT is set up.
+	cli				; Clear interrupts. Re-enable them once the IDT is set up.
 
 ; Set segments and stack.
 
-	xor cx, cx					; We will need cl cleared later.
+	xor cx, cx			; We will need cl cleared later.
 	mov ds, cx
 	mov es, cx
 	mov ss, cx
-	mov sp, 7A00h
+	mov sp, 0x7A00
 
-; Relocate self to 7A00h.
+; Relocate self to 0x7A00.
 
-	mov ch, 1					; Move 0x0100 words, cl is cleared.
-	mov si, 7C00h					; Source.
-	mov di, sp					; Destination. Bottom of relocation point is top of stack.
-	cld						; Ensure the Direction Flag points up for (future) string operations.
-rep	movsw						; Move words.
-	jmp 0:$+5					; Canonicalize cs:ip by far-jumping to relocated code.
+	mov ch, 1			; Move 0x100 words, cl is cleared.
+	mov si, 0x7C00			; Source.
+	mov di, sp			; Destination. Bottom of relocation point is top of stack.
+	cld				; Ensure the Direction Flag points up for (future) string operations.
+rep	movsw				; Move words.
+	jmp 0:$+5			; Canonicalize cs:ip by far-jumping to relocated code.
 
-; We will count active partitions in cx as we scan the partition table. cx = 0 from the last MOVSW.
-; Unrolled loop for simplicity.
+; We will count active partitions in cx as we scan the partition table. cx is clear from the last movsw. Unrolled loop for simplicity.
 
-	check 01BEh
-	check 01CEh
-	check 01DEh
-	check 01EEh
+	check 0x1BE
+	check 0x1CE
+	check 0x1DE
+	check 0x1EE
 
-	cmp cx, 1					; How many active partitions were there -
-	jl error0					; - too few?
-	jg error1					; - too many?
+	cmp cx, 1			; How many active partitions were there:
+	jl error0			; * too few?
+	jg error1			; * too many?
 
 ; Head.
 
@@ -51,7 +49,7 @@ rep	movsw						; Move words.
 
 	mov ax, [si+2]
 	mov cl, al
-	and cl, 3Fh
+	and cl, 0x3F
 
 ; Cylinder.
 
@@ -59,19 +57,19 @@ rep	movsw						; Move words.
 	shl ax, 1
 	mov ch, ah
 
-	mov ah, 0x02
-	mov al, 0x01
-	mov bx, 0x7C00					; Destination.
-	int 0x13					; Load sector.
+	mov ah, 2
+	mov al, 1
+	mov bx, 0x7C00			; Destination.
+	int 0x13			; Load sector.
 
-	jc error2					; Success?
+	jc error2			; Success?
 
-	cmp word [0x7DFE], 0xAA55			; Check boot signature.
+	cmp word [0x7DFE], 0xAA55	; Check VBR boot signature.
 	jne error3
 
 	jmp 0x7C00
 
-; At this point we should be at the start of VBR with DL set to the boot drive number and DS:SI pointing to the active partition entry.
+; At this point we should be at the start of Volume Boot Record with dl set to the Boot Drive number and ds:si pointing to the Active Partition entry.
 
 error0:	mov si, message0
 	jmp print
@@ -86,29 +84,29 @@ error3:	mov si, message3
 
 ; Fallthrough to print from which we do not return to simplify its implementation.
 
-print:	mov ah, 0x0E					; Select bios function.
-	mov bh, 0					; Page number.
-	mov bl, 0					; Foreground color.
-.loop:	lodsb						; Load byte to print.
-	or al, al					; If the byte is 0
-	jz hang						; hang
-	int 0x10					; else print the character
-	jmp .loop					; and loop.
+print:	mov ah, 0xE			; Select bios function.
+	mov bh, 0			; Page number.
+	mov bl, 0			; Foreground color.
+.loop:	lodsb				; Load byte to print.
+	or al, al			; If the byte is 0
+	jz hang				; hang
+	int 0x10			; else print the character
+	jmp .loop			; and loop.
 
 hang:	hlt
 
 message0: db "No active partition!",  0
 message1: db "More than one active partition!", 0
 
-times	220-($-$$) dd 0					; Disk timestamp.
+times	0xDA-($-$$) dd 0		; Pad up to Disk Timestamp.
+	dw 0				; Has to be zero as part of the Disk Timestamp.
+	dd 0				; Disk timestamp.
 
 message2: db "Can not read Volume Boot Record from disk!", 0
 message3: db "Volume Boot Record has wrong boot signature!", 0
 
-times	0x01B4-($-$$) db 0				; Fill with zeros up to the start of the data structures.
+times	0x1B4-($-$$) db 0		; Fill with zeros up to the start of the data structures.
+times	0xA db 0			; Optional unique disk ID.
+times	0x40 db 0			; Partition table.
 
-times	10 db 0						; Optional unique disk ID.
-
-times	8 dq 0						; Partition table.
-
-dw 0xAA55						; MBR boot signature.
+	dw 0xAA55			; MBR boot signature.
